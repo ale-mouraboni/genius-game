c486565d7629cc1131ecbd5cfc192cd68c195c28
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2020-11-30 21:21:48 -0300

    function

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,13 @@ const blue = document.querySelector('.blue')
 const red = document.querySelector('.red')
 const green = document.querySelector('.green')
 const yellow = document.querySelector('.yellow')
+
+let shuffleOrder = {} => {
+    let colorOrder = Math.floor(Math.random() * 4)
+    order[order.length] = colorOrder
+    clickedOrder = []
+
+    for(let i in order) {
+        let elementColor = createElement(order[i])
+    }
+}
