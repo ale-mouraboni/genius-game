9f1e19f297c6283be52beda2e3c6904ef09094ae
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2020-11-30 21:14:55 -0300

    variaveis

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let order = []
+let clickedOrder = []
+let score = 0
+
+// 0 = green
+// 1 = red
+// 2 = yellow
+// 3 = blue
+
+const blue = document.querySelector('.blue')
+const red = document.querySelector('.red')
+const green = document.querySelector('.green')
+const yellow = document.querySelector('.yellow')
